1ba8b6ab0df2bbb30340c242653befd57d6a49be
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2020-01-02 12:56:33 +0530

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
